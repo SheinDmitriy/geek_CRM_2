cf00629ac8df89a5f1fe0fac64f0835e0810ab65
1Zaytsev <4yu@bk.ru>
2020-08-31 00:28:59 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/team1/geek_crm/GeekCrmApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.team1.geek_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/team1/geek_crm/GeekCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.team1.geek_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
